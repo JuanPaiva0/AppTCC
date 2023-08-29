f8eaee861e208b16a8157732a2a5d359fb07b26a
Juan <juanpaiva887@gmail.com>
2023-08-29 18:27:19 -0300

    tela e métodos feitos
    tela de cadastro finalizada, métodos para a troca de telas feitos tambem

--- AppTCC/app/src/main/java/com/example/apptcc/MainActivity.java
+++ AppTCC/app/src/main/java/com/example/apptcc/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.apptcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        link = findViewById(R.id.link_cadastre_se);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mudarParaTelaDeCadastro();
+            }
+        });
+    }
+
+
+    public void mudarParaTelaDeCadastro(){
+       Intent it_telaCadastro = new Intent(this, Tela_cadastro.class);
+       startActivity(it_telaCadastro);
+
     }
 }
--- AppTCC/app/src/main/java/com/example/apptcc/Tela_cadastro.java
+++ AppTCC/app/src/main/java/com/example/apptcc/Tela_cadastro.java
@@ -2,13 +2,35 @@ package com.example.apptcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Tela_cadastro extends AppCompatActivity {
 
+    TextView link;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+
+       link = findViewById(R.id.link_login);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mudarParaTelaDeLogin();
+           }
+        });
     }
+
+    public void mudarParaTelaDeLogin(){
+       Intent it_mudarTela = new Intent(this, MainActivity.class);
+        startActivity(it_mudarTela);
+
+    }
+
 }
